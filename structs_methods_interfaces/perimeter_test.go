86119fe99bd892f1df16269db3677c67d431b1bf
Matthew <matthew_wheeler@live.co.uk>
2020-05-28 23:48:54 +0100

    Added, Structs, methods and interfaces.

---
+++ structs_methods_interfaces/perimeter_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+// func TestArea(t *testing.T) {
+
+// 	checkArea := func(t *testing.T, shape Shape, want float64) {
+// 		t.Helper()
+// 		got := shape.Area() // utalising an interface that enforces the type used must have a method called Area()
+// 		if got != want {
+// 			t.Errorf("got %g want %g", got, want)
+// 		}
+// 	}
+
+// 	t.Run("rectangles", func(t *testing.T) {
+// 		rectangle := Rectangle{12, 6}
+// 		checkArea(t, rectangle, 72.0)
+// 	})
+
+// 	t.Run("circles", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		checkArea(t, circle, 314.1592653589793)
+// 	})
+
+// }
+
+// table driven tests
+func TestArea(t *testing.T) {
+
+	// Create an array of tests that we want to perform
+	// using the struct to define what the test data consists of
+	// in this case, a type that follows the interface "Shape" and a "want" value
+	// This is known as an "anonymous struct"
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) { // t.run not nessasery but will give a better error if something fails.
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
+}
