7526d21a8d3feb2e047f1c3101309faa629d69bc
Matthew <matthew_wheeler@live.co.uk>
2020-05-28 15:46:39 +0100

    Arrays and slices.

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) (sum int) {
 
 	return sum
 }
+
+// ... this allows for a variable amount of parameters to be passed to the function
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, value := range numbersToSum {
+		sums = append(sums, Sum(value))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, value := range numbersToSum {
+		if len(value) > 1 {
+			numbers := value[1:] // create a slice of the value array from index 1 to the end.
+			sums = append(sums, Sum(numbers))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3}
@@ -11,5 +14,39 @@ func TestSum(t *testing.T) {
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+// Our next requirement is to change SumAll to SumAllTails, where it now calculates the totals of the "tails" of each slice.
+// The tail of a collection is all the items apart from the first one (the "head")
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
 
 }
